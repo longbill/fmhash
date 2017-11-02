54d233b2a2b0d29fdffe5f18ab29351c903a7fdd
Chunlong <longbill.cn@gmail.com>
2017-11-03 01:06:44 +0800

    improve performance

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const farmhash = require('farmhash');
+const farmhash = require('farmhash').fingerprint32;
 const base62 = require('base62').encode;
 
 module.exports = function(b) {
-	if (!Buffer.isBuffer(b)) b = new Buffer(b);
-	let hash = base62(farmhash.fingerprint32( b.slice(0, Math.ceil(b.length / 2)) )) + base62(farmhash.fingerprint32( b ));
+	let hash = base62(farmhash( b.slice(0, Math.ceil(b.length / 2)) )) + base62(farmhash( b ));
 	while (hash.length < 12) hash += '0';
 	return hash;
 };
